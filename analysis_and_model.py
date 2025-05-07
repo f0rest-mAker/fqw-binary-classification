6dedb7ea23eaeebb0e86d837c92919eadbf1613d
ruzal <f0restName@email.com>
2025-05-08 00:10:50 +0300

    added streamlit app

--- analysis_and_model.py
+++ analysis_and_model.py
@@ -1,1 +1,89 @@
+from sklearn.preprocessing import LabelEncoder
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, roc_auc_score
 
+
+def analysis_and_model_page():
+    st.title("Анализ данных и модель")
+    uploaded_file = st.file_uploader("Загрузите датасет (CSV)", type="csv")
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+        features = data[[
+            "Type",
+            "Air temperature [K]",
+            "Process temperature [K]",
+            "Rotational speed [rpm]",
+            "Torque [Nm]",
+            "Tool wear [min]"
+        ]].copy()
+        encoder = LabelEncoder()
+        features["Type"] = encoder.fit_transform(
+            features["Type"]).copy()
+        features_for_standard_scaler = ["Air temperature [K]", "Process temperature [K]",
+                                        "Rotational speed [rpm]", "Torque [Nm]", "Tool wear [min]"]
+        scaler = StandardScaler()
+        features[features_for_standard_scaler] = scaler.fit_transform(
+            features[features_for_standard_scaler])
+        target = data["Machine failure"]
+        X = features
+        y = target
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42)
+        model = DecisionTreeClassifier()
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        conf_matrix = confusion_matrix(y_test, y_pred)
+        classification_rep = classification_report(y_test, y_pred)
+
+        st.header("Результаты обучения модели")
+        st.write(f"Accuracy: {accuracy:.2f}")
+
+        st.subheader("Confusion Matrix")
+        fig, ax = plt.subplots()
+        sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', ax=ax)
+        st.pyplot(fig)
+
+        st.subheader("Classification Report")
+        st.text(classification_rep)
+
+        st.header("Предсказание по новым данным")
+        with st.form("prediction_form"):
+            st.write("Введите значения признаков для предсказания:")
+            type = st.selectbox("type", ["L", "M", "H"])
+            air_temp = st.number_input("air temperature [K]")
+            process_temp = st.number_input("process temperature [K]")
+            rotational_speed = st.number_input("rotational speed [rpm]")
+            torque = st.number_input("torque [Nm]")
+            tool_wear = st.number_input("tool wear [min]")
+
+            submit_button = st.form_submit_button("Предсказать")
+
+            if submit_button:
+                input_data = pd.DataFrame({
+                    "Type": encoder.transform([type]),
+                    "Air temperature [K]": air_temp,
+                    "Process temperature [K]": process_temp,
+                    "Rotational speed [rpm]": rotational_speed,
+                    "Torque [Nm]": torque,
+                    "Tool wear [min]": tool_wear
+                })
+
+                input_data[features_for_standard_scaler] = scaler.transform(
+                    input_data[features_for_standard_scaler])
+
+                prediction = model.predict(input_data)
+                prediction_proba = model.predict_proba(input_data)[:, 1]
+
+                st.write(f"Предсказание: {prediction[0]}")
+                st.write(f"Вероятность отказа: {prediction_proba[0]:.5f}")
+
+
+analysis_and_model_page()
---
+++ presentation.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import reveal_slides as rs
+
+
+def presentation_page():
+    st.title("Презентация проекта")
+
+    presentation_markdown = """
+    # Прогнозирование отказов оборудования
+    ---
+    ## Введение
+    - Нашей задачей является построение модели для предсказания отказа оборудования. Обучение модели будет происходить на основе датасета, который содержит параметры оборудования во время работы и результат сбоя(1 - да, 0 - нет).
+    - Цель: предсказать отказ оборудования (Target = 1) или его отсутствие (Target = 0).
+    ---
+    ## Этапы работы
+    1. Загрузка данных.
+    2. Предобработка данных.
+    3. Обучение модели.
+    4. Оценка модели.
+    5. Визуализация результатов.
+    ---
+    ## Streamlit-приложение
+    - Основная страница: анализ данных и предсказания.
+    - Страница с презентацией: описание проекта.
+    ---
+    ## Заключение
+    - Была построена модель для предсказания отказа оборудования. Однако данную модель можно улучшить, подобрав лучшие гиперпараметры с помощью изученных во время курса методов, либо попробовать найти другую модель, которая будет лучше предсказывать сбои.
+    """
+
+    with st.sidebar:
+        st.header("Настройки презентации")
+        theme = st.selectbox("Тема", ["black", "white", "league", "beige",
+                                      "sky", "night", "serif", "simple", "solarized"])
+        height = st.number_input("Высота слайдов", value=500)
+        transition = st.selectbox("Переход", ["slide", "convex", "concave",
+                                              "zoom", "none"])
+        plugins = st.multiselect("Плагины", ["highlight", "katex",
+                                             "mathjax2", "mathjax3", "notes", "search", "zoom"], [])
+
+    rs.slides(
+        presentation_markdown,
+        height=height,
+        theme=theme,
+        config={
+            "transition": transition,
+            "plugins": plugins,
+        },
+        markdown_props={"data-separator-vertical": "^--$"},
+    )
+
+
+presentation_page()
